a8457ae165488dff69c5a48c7fb3c0a7ed1e788a
Mayushll <inversionspells@mail.ru>
2021-12-05 19:40:27 +0500

    Added profile picture, remaked Profile/Spoiler styles

--- src/components/Profile/MyProfile/MyProfile.jsx
+++ src/components/Profile/MyProfile/MyProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatar from "./Avatar.png"
+import avatar from "./Avatar.webp"
 import styles from "./MyProfile.module.scss"
 import {getAge} from "../../../Functions/getAge";
 import {Spoilers} from "./Spoilers/Spoilers";
@@ -8,14 +8,16 @@ export const MyProfile = () => {
         <div>
             <div className={styles.profile}>
                 <div>
-                    <img className={styles.avatar} src={avatar} alt="Аватар"/>
+                    <img
+                        className={styles.avatar} src={avatar} alt="Аватар"/>
                 </div>
                 <div>
                     <span> <b> Возьмите на работу пожалуйста </b> </span><br/>
                     Бородин Владислав <br/>
                     {getAge("2003.6.27")} <br/>
                     <a className={styles.url} href="https://wa.me/+79193930621">WhatsApp</a> <br />
-                    <a className={styles.url} href="tel:+79193930621">+7-919-393-06-21</ a>
+                    <a className={styles.url} href="tel:+79193930621">+7-919-393-06-21</ a> <br />
+                    <a className={styles.url} href="https://vk.com/mayushiides">ВКонтакте</ a>
                 </div>
             </div>
             <br />
