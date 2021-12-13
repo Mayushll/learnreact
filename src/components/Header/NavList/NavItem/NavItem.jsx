69419b6e10994b0140b40f71c8e668af20111da2
Mayushll <inversionspells@mail.ru>
2021-12-13 13:30:46 +0500

    Reworked NavLink styles and some hover styles

--- src/components/Header/NavList/NavItem/NavItem.jsx
+++ src/components/Header/NavList/NavItem/NavItem.jsx
@@ -8,19 +8,11 @@ import ThemeContext from "../../../../Context/ThemeContext";
 export function NavItem(props) {
     const contextTheme = useContext(ThemeContext)
     return (
-        <div>
             <NavLink className={
-                contextTheme.theme === "dark"
-                    ? styles.itemDark
-                    : styles.itemWhite
+                contextTheme.theme === "light"
+                    ? styles.itemWhite
+                    : styles.itemDark
             }
-                     style={({ isActive }) => ({
-                         color: contextTheme.theme === "dark" ?
-                             isActive ? "rgb(30, 144, 255)" : "rgb(220,220,220)" :
-                             isActive ? "rgb(30, 144, 255)" : "rgb(20, 20, 20)",
-                         }
-                         )}
                      to={"/"+ props.post.url}>{props.post.title}</NavLink>
-        </div>
     );
 }
